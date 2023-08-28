60d5f56c4acc905c673ca64e2e0c52953862bf94
Adam Buffett <adam_buffett@outlook.com>
2023-08-28 16:43:53 -0400

    Set up vue-router

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'NavBar'
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h1>Welcome</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HomePage"
+}
+</script>
