c5e2288652190b545785370eb586e6f69fda0a9d
Adam Buffett <adam_buffett@outlook.com>
2023-09-28 14:55:18 -0400

    Created CocktailCard component

---
+++ src/components/CoktailCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="card">
+    <img :src="cocktail.strDrinkThumb" :alt="cocktail.strDrink">
+    <h3>{{ cocktail.strDrink }}</h3>
+    <p>{{ cocktail.strCategory }}</p>
+    <p v-if="cocktail.author">By: {{ cocktail.author }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CocktailDetail',
+    data: () => ({
+        cocktail: {}
+    })
+}
+</script>
+
+<style scoped>
+.card {
+  height: 400px;
+  width: 342px;
+  border-radius: 25px;
+  
+}
+
+img {
+  height: 342px;
+  border-top-left-radius: 25px;
+  border-top-right-radius: 25px;
+}
+
+</style>
