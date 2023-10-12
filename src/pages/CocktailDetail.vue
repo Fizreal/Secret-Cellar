e90f9431e40ac2aed58df8f26e7d1ca5fa16c51a
Adam Buffett <adam_buffett@outlook.com>
2023-10-12 15:48:34 -0400

    Create methods to update language and directions

--- src/pages/CocktailDetail.vue
+++ src/pages/CocktailDetail.vue
@@ -4,14 +4,14 @@
         <img :src="cocktail.image_url" :alt="cocktail.name">
         <section id="ingredients">
             <h2>Ingredients</h2>
-            <ul v-for="ingredient in cocktail.ingredients" :key="ingredient._id">
-                <li><span v-if="ingredient.measure">{{ ingredient.measure }}&nbsp;</span>{{ ingredient.name }}</li>
+            <ul >
+                <li v-for="ingredient in cocktail.ingredients" :key="ingredient._id"><span v-if="ingredient.measure">{{ ingredient.measure }}&nbsp;</span>{{ ingredient.name }}</li>
             </ul>
         </section>
         <section>
             <h2>Instructions</h2>
-            <div v-for="instruction in cocktail.instructions" :key="instruction.language">
-                <button :id="instruction.language"></button>
+            <div >
+                <button :id="instruction.language" v-for="instruction in cocktail.instructions" :key="instruction.language" @click="() => setLanguage(instruction.language)"></button>
             </div>
             <p>{{ directions }}</p>
             <h3>Recommended glass: {{ cocktail.glass }}</h3>
@@ -25,16 +25,32 @@ export default {
     name: 'CocktailDetail',
     data: () => ({
         cocktail: {},
-        instructions: 'en',
+        language: '',
         directions: ''
     }),
     mounted() {
-        this.getCocktail();
+        this.getCocktail()
+    },
+    watch: {
+        language() {
+            this.setDirections()
+        }
     },
     methods: {
         async getCocktail() {
             let cocktailDetails = await getCocktailDetails(this.$route.params.cocktailId)
-            this.cocktail = cocktailDetails
+            this.cocktail = cocktailDetails.data
+            this.language = cocktailDetails.data.instructions[0].language
+        },
+        setLanguage(language) {
+            this.language = language
+        },
+        setDirections() {
+            const findLanguageIndex = (instruction) => instruction.language === this.language
+            let idx = this.cocktail.instructions.findIndex(findLanguageIndex)
+            if (idx !== -1) {
+                this.directions = this.cocktail.instructions[idx].steps
+            }
         }
     },
 }
